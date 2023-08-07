a219e51d1fecc9a8a000835f07b70c2b965e198e
MaxGoxt <135187058+MaxGoxt@users.noreply.github.com>
2023-08-07 20:59:01 +0000

    commit

---
+++ src/js/component/cards.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div className="card col-3 container p-0" style={{width: '18rem'}}>
+            <img src="https://i.pinimg.com/236x/cc/c4/06/ccc406dbafc09f3ac2f066a494af21e7.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title text-center">Card title</h5>
+                <p className="card-text text-center">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div className="card-footer d-flex justify-content-center">
+                <button className="btn btn-primary">Find Out More!</button>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
---
+++ src/js/component/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="navbar navbar-expand-lg bg-body-tertiary bg-dark">
+  <div className="m-auto"><small className="text-white">Copyright © your Webside 2023</small></div></footer>
+    )
+}
+
+export default Footer;
